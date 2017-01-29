93c31ac8783e4444dc79ad7746230b711e218cba
N. Shibeko <nikit.shibeko@gmail.com>
2017-01-29 16:41:57 +0300

    new1

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.min = 0;
+    	this.max = 0;
+    	this game = 0;
+    	return this;
+    }
 
     setRange(min, max) {
-
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+      this.game = math.floor((this.min + this.max) / 2);
+      return game;
     }
 
     lower() {
-
+    	 this.max = this.game;
     }
 
     greater() {
-
+    	this.min = this.game;
     }
 }
 
